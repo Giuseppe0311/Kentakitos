5af7a5db039f46b3053f8c6091ecf99eb5875ccd
Giuseppe0311 <barrerita_romero@hotmail.com>
2023-06-08 21:01:01 -0500

    Actualizacion

--- src/routes/Index.routes.js
+++ src/routes/Index.routes.js
@@ -14,7 +14,7 @@ const router = Router();
 // Rutas
 router.get("/", (req, res) => {
   valorlegueado = req.isAuthenticated();
-  res.render("index", { usuarioEstaLogueado: valorlegueado });
+  res.send("hola");
 });
 // Monta el enrutador en la aplicación
 
